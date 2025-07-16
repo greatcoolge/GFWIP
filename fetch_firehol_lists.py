dd3ac088c65116ead3b9c9465f3cd33bb74ff4d1
greatcoolge <121862222+greatcoolge@users.noreply.github.com>
2025-07-16 23:42:53 +0800

    fetch_firehol_lists.py

--- fetch_firehol_lists.py
+++ fetch_firehol_lists.py
@@ -10,7 +10,7 @@ LISTS_V4 = {
     "firehol_level1":    "https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/firehol_level1.netset",
     "spamhaus_drop":     "https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/spamhaus_drop.netset",
     #"compromised-ips":   "http://rules.emergingthreats.net/blockrules/compromised-ips.txt",
-    "dshield":           "https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/dshield.netset",
+    #"dshield":           "https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/dshield.netset",
     "feodo":             "https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/feodo.netset",
     "sslbl":             "https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/sslbl.netset",
     "zeus_badips":       "https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/zeus_badips.netset",
