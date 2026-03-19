35738396db3965d27868b161b681354673127acc
greatcoolge <121862222+greatcoolge@users.noreply.github.com>
2026-03-19 14:01:02 +0800

    Update fetch_firehol_lists.py

--- fetch_firehol_lists.py
+++ fetch_firehol_lists.py
@@ -18,9 +18,9 @@ LISTS_V4 = {
     "abuse_palevo": "https://raw.githubusercontent.com/firehol/blocklist-ipsets/refs/heads/master/iblocklist_abuse_palevo.netset",
     "dshield": "https://feeds.dshield.org/block.txt",
     "feodo": "https://feodotracker.abuse.ch/downloads/ipblocklist_recommended.txt",
-    "sslbl": "https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/sslbl.netset",
+    # "sslbl": "https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/sslbl.netset",
     # "Blocklist.de": "https://lists.blocklist.de/lists/strongips.txt",
-    "zeus_badips": "https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/zeus_badips.netset",
+    "zeus_badips": "https://raw.githubusercontent.com/firehol/blocklist-ipsets/refs/heads/master/iblocklist_abuse_zeus.netset",
     "bogons": "http://www.cidr-report.org/bogons/freespace-prefix.txt",
 }
 
